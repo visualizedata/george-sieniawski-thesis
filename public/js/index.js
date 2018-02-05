c2a6acf37105236eb18234b551c24b482fbd55d0
George S <31370146+sieng146@users.noreply.github.com>
2018-02-05 13:00:58 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 var $cont = document.querySelector('.cont');
 var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-var $closeBtnsArr = [].slice.call(document.querySelectorAll('.individualsection__close-btn'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
 
 setTimeout(function() {
   $cont.classList.remove('s--inactive');
